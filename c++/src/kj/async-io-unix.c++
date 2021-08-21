sobomax/capnproto

commit d3b0dc2098ef5634873774b2260bd52b1f3c9c7d

Extend basic network interfaces with PeerIdentity.
This:
1. Provides a cleaner interface for identifying connecting clients, compared to the raw `getpeername()` method, which should now probably be deprecated.
2. Provides a hook where we can hang TLS certificate information (coming in a later commit).

c++/src/kj/async-io-unix.c++
@@ -888,6 +888,10 @@ public:
     return filter.shouldAllowParse(&addr.generic, addrlen);
   }
 
+  kj::Own<PeerIdentity> getIdentity(LowLevelAsyncIoProvider& llaiop,
+                                    LowLevelAsyncIoProvider::NetworkFilter& filter,
+                                    AsyncIoStream& stream) const;
+
 private:
   SocketAddress() {
     // We need to memset the whole object 0 otherwise Valgrind gets unhappy when we write it to a
@@ -1059,12 +1063,21 @@ Promise<Array<SocketAddress>> SocketAddress::lookupHost(
 
 class FdConnectionReceiver final: public ConnectionReceiver, public OwnedFileDescriptor {
 public:
-  FdConnectionReceiver(UnixEventPort& eventPort, int fd,
+  FdConnectionReceiver(LowLevelAsyncIoProvider& lowLevel,
+                       UnixEventPort& eventPort, int fd,
                        LowLevelAsyncIoProvider::NetworkFilter& filter, uint flags)
-      : OwnedFileDescriptor(fd, flags), eventPort(eventPort), filter(filter),
+      : OwnedFileDescriptor(fd, flags), lowLevel(lowLevel), eventPort(eventPort), filter(filter),
         observer(eventPort, fd, UnixEventPort::FdObserver::OBSERVE_READ) {}
 
   Promise<Own<AsyncIoStream>> accept() override {
+    return acceptImpl(false).then([](AuthenticatedStream&& a) { return kj::mv(a.stream); });
+  }
+
+  Promise<AuthenticatedStream> acceptAuthenticated() override {
+    return acceptImpl(true);
+  }
+
+  Promise<AuthenticatedStream> acceptImpl(bool authenticated) {
     int newFd;
 
     struct sockaddr_storage addr;
@@ -1079,12 +1092,18 @@ public:
 #endif
 
     if (newFd >= 0) {
+      kj::AutoCloseFd ownFd(newFd);
       if (!filter.shouldAllow(reinterpret_cast<struct sockaddr*>(&addr), addrlen)) {
-        // Drop disallowed address.
-        close(newFd);
-        return accept();
+        // Ignore disallowed address.
+        return acceptImpl(authenticated);
       } else {
-        return Own<AsyncIoStream>(heap<AsyncStreamFd>(eventPort, newFd, NEW_FD_FLAGS));
+        AuthenticatedStream result;
+        result.stream = heap<AsyncStreamFd>(eventPort, ownFd.release(), NEW_FD_FLAGS);
+        if (authenticated) {
+          result.peerIdentity = SocketAddress(reinterpret_cast<struct sockaddr*>(&addr), addrlen)
+              .getIdentity(lowLevel, filter, *result.stream);
+        }
+        return kj::mv(result);
       }
     } else {
       int error = errno;
@@ -1095,8 +1114,8 @@ public:
         case EWOULDBLOCK:
 #endif
           // Not ready yet.
-          return observer.whenBecomesReadable().then([this]() {
-            return accept();
+          return observer.whenBecomesReadable().then([this,authenticated]() {
+            return acceptImpl(authenticated);
           });
 
         case EINTR:
@@ -1142,6 +1161,7 @@ public:
   }
 
 public:
+  LowLevelAsyncIoProvider& lowLevel;
   UnixEventPort& eventPort;
   LowLevelAsyncIoProvider::NetworkFilter& filter;
   UnixEventPort::FdObserver observer;
@@ -1239,7 +1259,7 @@ public:
   }
   Own<ConnectionReceiver> wrapListenSocketFd(
       int fd, NetworkFilter& filter, uint flags = 0) override {
-    return heap<FdConnectionReceiver>(eventPort, fd, filter, flags);
+    return heap<FdConnectionReceiver>(*this, eventPort, fd, filter, flags);
   }
   Own<DatagramPort> wrapDatagramSocketFd(
       int fd, NetworkFilter& filter, uint flags = 0) override {
@@ -1267,7 +1287,14 @@ public:
 
   Promise<Own<AsyncIoStream>> connect() override {
     auto addrsCopy = heapArray(addrs.asPtr());
-    auto promise = connectImpl(lowLevel, filter, addrsCopy);
+    auto promise = connectImpl(lowLevel, filter, addrsCopy, false);
+    return promise.attach(kj::mv(addrsCopy))
+        .then([](AuthenticatedStream&& a) { return kj::mv(a.stream); });
+  }
+
+  Promise<AuthenticatedStream> connectAuthenticated() override {
+    auto addrsCopy = heapArray(addrs.asPtr());
+    auto promise = connectImpl(lowLevel, filter, addrsCopy, true);
     return promise.attach(kj::mv(addrsCopy));
   }
 
@@ -1339,10 +1366,11 @@ private:
   Array<SocketAddress> addrs;
   uint counter = 0;
 
-  static Promise<Own<AsyncIoStream>> connectImpl(
+  static Promise<AuthenticatedStream> connectImpl(
       LowLevelAsyncIoProvider& lowLevel,
       LowLevelAsyncIoProvider::NetworkFilter& filter,
-      ArrayPtr<SocketAddress> addrs) {
+      ArrayPtr<SocketAddress> addrs,
+      bool authenticated) {
     KJ_ASSERT(addrs.size() > 0);
 
     return kj::evalNow([&]() -> Promise<Own<AsyncIoStream>> {
@@ -1353,14 +1381,21 @@ private:
         return lowLevel.wrapConnectingSocketFd(
             fd, addrs[0].getRaw(), addrs[0].getRawSize(), NEW_FD_FLAGS);
       }
-    }).then([](Own<AsyncIoStream>&& stream) -> Promise<Own<AsyncIoStream>> {
+    }).then([&lowLevel,&filter,addrs,authenticated](Own<AsyncIoStream>&& stream)
+        -> Promise<AuthenticatedStream> {
       // Success, pass along.
-      return kj::mv(stream);
-    }, [&lowLevel,&filter,addrs](Exception&& exception) mutable -> Promise<Own<AsyncIoStream>> {
+      AuthenticatedStream result;
+      result.stream = kj::mv(stream);
+      if (authenticated) {
+        result.peerIdentity = addrs[0].getIdentity(lowLevel, filter, *result.stream);
+      }
+      return kj::mv(result);
+    }, [&lowLevel,&filter,addrs,authenticated](Exception&& exception) mutable
+        -> Promise<AuthenticatedStream> {
       // Connect failed.
       if (addrs.size() > 1) {
         // Try the next address instead.
-        return connectImpl(lowLevel, filter, addrs.slice(1, addrs.size()));
+        return connectImpl(lowLevel, filter, addrs.slice(1, addrs.size()), authenticated);
       } else {
         // No more addresses to try, so propagate the exception.
         return kj::mv(exception);
@@ -1368,6 +1403,61 @@ private:
     });
   }
 };
+
+kj::Own<PeerIdentity> SocketAddress::getIdentity(kj::LowLevelAsyncIoProvider& llaiop,
+                                                 LowLevelAsyncIoProvider::NetworkFilter& filter,
+                                                 AsyncIoStream& stream) const {
+  switch (addr.generic.sa_family) {
+    case AF_INET:
+    case AF_INET6:
+      return NetworkPeerIdentity::newInstance(
+          kj::heap<NetworkAddressImpl>(llaiop, filter, kj::arr(*this)));
+    case AF_UNIX: {
+      LocalPeerIdentity::Credentials result;
+
+      // There is little documentation on what happens when the uid/pid can't be obtained, but I've
+      // seen vague references on the internet saying that a PID of 0 and a UID of uid_t(-1) are used
+      // as invalid values.
+
+#if defined(SO_PEERCRED)
+      struct ucred creds;
+      uint length = sizeof(creds);
+      stream.getsockopt(SOL_SOCKET, SO_PEERCRED, &creds, &length);
+      if (creds.pid > 0) {
+        result.pid = creds.pid;
+      }
+      if (creds.uid != static_cast<uid_t>(-1)) {
+        result.uid = creds.uid;
+      }
+
+#elif defined(LOCAL_PEERCRED)
+      // MacOS / FreeBSD
+      struct xucred creds;
+      uint length = sizeof(creds);
+      stream.getsockopt(SOL_LOCAL, LOCAL_PEERCRED, &creds, &length);
+      KJ_ASSERT(length == sizeof(creds));
+      if (creds.sc_uid != static_cast<uid_t>(-1)) {
+        result.uid = creds.sc_uid;
+      }
+
+#if defined(LOCAL_PEERPID)
+      // MacOS only?
+      pid_t pid;
+      length = sizeof(pid);
+      stream.getsockopt(socketFd, SOL_LOCAL, LOCAL_PEERCRED, &creds, &length);
+      KJ_ASSERT(length == sizeof(pid));
+      if (pid > 0) {
+        result.pid = pid;
+      }
+#endif
+#endif
+
+      return LocalPeerIdentity::newInstance(result);
+    }
+    default:
+      return UnknownPeerIdentity::newInstance();
+  }
+}
 
 class SocketNetwork final: public Network {
 public:

c++/src/kj/async-io-test.c++
@@ -89,6 +89,153 @@ TEST(AsyncIo, SimpleNetwork) {
   EXPECT_EQ("foo", result);
 }
 
+TEST(AsyncIo, SimpleNetworkAuthentication) {
+  auto ioContext = setupAsyncIo();
+  auto& network = ioContext.provider->getNetwork();
+
+  Own<ConnectionReceiver> listener;
+  Own<AsyncIoStream> server;
+  Own<AsyncIoStream> client;
+
+  char receiveBuffer[4];
+
+  auto port = newPromiseAndFulfiller<uint>();
+
+  port.promise.then([&](uint portnum) {
+    return network.parseAddress("localhost", portnum);
+  }).then([&](Own<NetworkAddress>&& addr) {
+    auto promise = addr->connectAuthenticated();
+    return promise.then([&,addr=kj::mv(addr)](AuthenticatedStream result) mutable {
+      auto id = result.peerIdentity.downcast<NetworkPeerIdentity>();
+
+      // `addr` was resolved from `localhost` and may contain multiple addresses, but
+      // result.peerIdentity tells us the specific address that was used. So it should be one
+      // of the ones on the list, but only one.
+      KJ_EXPECT(strstr(addr->toString().cStr(), id->getAddress().toString().cStr()) != nullptr);
+      KJ_EXPECT(id->getAddress().toString().findFirst(',') == nullptr);
+
+      client = kj::mv(result.stream);
+
+      // `id` should match client->getpeername().
+      union {
+        struct sockaddr generic;
+        struct sockaddr_in ip4;
+        struct sockaddr_in6 ip6;
+      } rawAddr;
+      uint len = sizeof(rawAddr);
+      client->getpeername(&rawAddr.generic, &len);
+      auto peername = network.getSockaddr(&rawAddr.generic, len);
+      KJ_EXPECT(id->toString() == peername->toString());
+
+      return client->write("foo", 3);
+    });
+  }).detach([](kj::Exception&& exception) {
+    KJ_FAIL_EXPECT(exception);
+  });
+
+  kj::String result = network.parseAddress("*").then([&](Own<NetworkAddress>&& result) {
+    listener = result->listen();
+    port.fulfiller->fulfill(listener->getPort());
+    return listener->acceptAuthenticated();
+  }).then([&](AuthenticatedStream result) {
+    auto id = result.peerIdentity.downcast<NetworkPeerIdentity>();
+    server = kj::mv(result.stream);
+
+    // `id` should match server->getpeername().
+    union {
+      struct sockaddr generic;
+      struct sockaddr_in ip4;
+      struct sockaddr_in6 ip6;
+    } addr;
+    uint len = sizeof(addr);
+    server->getpeername(&addr.generic, &len);
+    auto peername = network.getSockaddr(&addr.generic, len);
+    KJ_EXPECT(id->toString() == peername->toString());
+
+    return server->tryRead(receiveBuffer, 3, 4);
+  }).then([&](size_t n) {
+    EXPECT_EQ(3u, n);
+    return heapString(receiveBuffer, n);
+  }).wait(ioContext.waitScope);
+
+  EXPECT_EQ("foo", result);
+}
+
+#if !_WIN32
+TEST(AsyncIo, UnixSocket) {
+  auto ioContext = setupAsyncIo();
+  auto& network = ioContext.provider->getNetwork();
+
+  auto path = kj::str("/tmp/kj-async-io-test.", getpid());
+  KJ_DEFER(unlink(path.cStr()));
+
+  Own<ConnectionReceiver> listener;
+  Own<AsyncIoStream> server;
+  Own<AsyncIoStream> client;
+
+  char receiveBuffer[4];
+
+  auto ready = newPromiseAndFulfiller<void>();
+
+  ready.promise.then([&]() {
+    return network.parseAddress(kj::str("unix:", path));
+  }).then([&](Own<NetworkAddress>&& addr) {
+    auto promise = addr->connectAuthenticated();
+    return promise.then([&,addr=kj::mv(addr)](AuthenticatedStream result) mutable {
+      auto id = result.peerIdentity.downcast<LocalPeerIdentity>();
+      auto creds = id->getCredentials();
+      KJ_IF_MAYBE(p, creds.pid) {
+        KJ_EXPECT(*p == getpid());
+#if __linux__ || __APPLE__
+      } else {
+        KJ_FAIL_EXPECT("LocalPeerIdentity for unix socket had null PID");
+#endif
+      }
+      KJ_IF_MAYBE(u, creds.uid) {
+        KJ_EXPECT(*u == getuid());
+      } else {
+        KJ_FAIL_EXPECT("LocalPeerIdentity for unix socket had null UID");
+      }
+
+      client = kj::mv(result.stream);
+      return client->write("foo", 3);
+    });
+  }).detach([](kj::Exception&& exception) {
+    KJ_FAIL_EXPECT(exception);
+  });
+
+  kj::String result = network.parseAddress(kj::str("unix:", path))
+      .then([&](Own<NetworkAddress>&& result) {
+    listener = result->listen();
+    ready.fulfiller->fulfill();
+    return listener->acceptAuthenticated();
+  }).then([&](AuthenticatedStream result) {
+    auto id = result.peerIdentity.downcast<LocalPeerIdentity>();
+    auto creds = id->getCredentials();
+    KJ_IF_MAYBE(p, creds.pid) {
+      KJ_EXPECT(*p == getpid());
+#if __linux__ || __APPLE__
+    } else {
+      KJ_FAIL_EXPECT("LocalPeerIdentity for unix socket had null PID");
+#endif
+    }
+    KJ_IF_MAYBE(u, creds.uid) {
+      KJ_EXPECT(*u == getuid());
+    } else {
+      KJ_FAIL_EXPECT("LocalPeerIdentity for unix socket had null UID");
+    }
+
+    server = kj::mv(result.stream);
+    return server->tryRead(receiveBuffer, 3, 4);
+  }).then([&](size_t n) {
+    EXPECT_EQ(3u, n);
+    return heapString(receiveBuffer, n);
+  }).wait(ioContext.waitScope);
+
+  EXPECT_EQ("foo", result);
+}
+#endif
+
 String tryParse(WaitScope& waitScope, Network& network, StringPtr text, uint portHint = 0) {
   return network.parseAddress(text, portHint).wait(waitScope)->toString();
 }

c++/src/kj/async-io.c++
@@ -2663,6 +2663,12 @@ Promise<Own<AsyncIoStream>> CapabilityStreamConnectionReceiver::accept() {
   });
 }
 
+Promise<AuthenticatedStream> CapabilityStreamConnectionReceiver::acceptAuthenticated() {
+  return accept().then([](Own<AsyncIoStream>&& stream) {
+    return AuthenticatedStream { kj::mv(stream), UnknownPeerIdentity::newInstance() };
+  });
+}
+
 uint CapabilityStreamConnectionReceiver::getPort() {
   return 0;
 }
@@ -2679,6 +2685,11 @@ Promise<Own<AsyncIoStream>> CapabilityStreamNetworkAddress::connect() {
       .then(kj::mvCapture(result, [](Own<AsyncIoStream>&& result) {
     return kj::mv(result);
   }));
+}
+Promise<AuthenticatedStream> CapabilityStreamNetworkAddress::connectAuthenticated() {
+  return connect().then([](Own<AsyncIoStream>&& stream) {
+    return AuthenticatedStream { kj::mv(stream), UnknownPeerIdentity::newInstance() };
+  });
 }
 Own<ConnectionReceiver> CapabilityStreamNetworkAddress::listen() {
   return kj::heap<CapabilityStreamConnectionReceiver>(inner);
@@ -3033,4 +3044,81 @@ bool NetworkFilter::shouldAllowParse(const struct sockaddr* addr, uint addrlen) 
 }
 
 }  // namespace _ (private)
+
+// =======================================================================================
+// PeerIdentity implementations
+
+namespace {
+
+class NetworkPeerIdentityImpl final: public NetworkPeerIdentity {
+public:
+  NetworkPeerIdentityImpl(kj::Own<NetworkAddress> addr): addr(kj::mv(addr)) {}
+
+  kj::String toString() override { return addr->toString(); }
+  NetworkAddress& getAddress() override { return *addr; }
+
+private:
+  kj::Own<NetworkAddress> addr;
+};
+
+class LocalPeerIdentityImpl final: public LocalPeerIdentity {
+public:
+  LocalPeerIdentityImpl(Credentials creds): creds(creds) {}
+
+  kj::String toString() override {
+    char pidBuffer[16];
+    kj::StringPtr pidStr = nullptr;
+    KJ_IF_MAYBE(p, creds.pid) {
+      pidStr = strPreallocated(pidBuffer, " pid:", *p);
+    }
+
+    char uidBuffer[16];
+    kj::StringPtr uidStr = nullptr;
+    KJ_IF_MAYBE(u, creds.uid) {
+      uidStr = strPreallocated(uidBuffer, " uid:", *u);
+    }
+
+    return kj::str("(local peer", pidStr, uidStr, ")");
+  }
+
+  Credentials getCredentials() override { return creds; }
+
+private:
+  Credentials creds;
+};
+
+class UnknownPeerIdentityImpl final: public UnknownPeerIdentity {
+public:
+  kj::String toString() override {
+    return kj::str("(unknown peer)");
+  }
+};
+
+}  // namespace
+
+kj::Own<NetworkPeerIdentity> NetworkPeerIdentity::newInstance(kj::Own<NetworkAddress> addr) {
+  return kj::heap<NetworkPeerIdentityImpl>(kj::mv(addr));
+}
+
+kj::Own<LocalPeerIdentity> LocalPeerIdentity::newInstance(LocalPeerIdentity::Credentials creds) {
+  return kj::heap<LocalPeerIdentityImpl>(creds);
+}
+
+kj::Own<UnknownPeerIdentity> UnknownPeerIdentity::newInstance() {
+  static UnknownPeerIdentityImpl instance;
+  return { &instance, NullDisposer::instance };
+}
+
+Promise<AuthenticatedStream> ConnectionReceiver::acceptAuthenticated() {
+  return accept().then([](Own<AsyncIoStream> stream) {
+    return AuthenticatedStream { kj::mv(stream), UnknownPeerIdentity::newInstance() };
+  });
+}
+
+Promise<AuthenticatedStream> NetworkAddress::connectAuthenticated() {
+  return connect().then([](Own<AsyncIoStream> stream) {
+    return AuthenticatedStream { kj::mv(stream), UnknownPeerIdentity::newInstance() };
+  });
+}
+
 }  // namespace kj

c++/src/kj/async-io.h
@@ -299,12 +299,114 @@ Own<AsyncIoStream> newPromisedStream(Promise<Own<AsyncIoStream>> promise);
 // Constructs an Async*Stream which waits for a promise to resolve, then forwards all calls to the
 // promised stream.
 
+// =======================================================================================
+// Authenticated streams
+
+class PeerIdentity {
+  // PeerIdentity provides information about a connecting client. Various subclasses exist to
+  // address different network types.
+public:
+  virtual kj::String toString() = 0;
+  // Returns a human-readable string identifying the peer. Where possible, this string will be
+  // in the same format as the addresses you could pass to `kj::Network::parseAddress()`. However,
+  // only certain subclasses of `PeerIdentity` guarantee this property.
+};
+
+struct AuthenticatedStream {
+  // A pair of an `AsyncIoStream` and a `PeerIdentity`. This is used as the return type of
+  // `NetworkAddress::connectAuthenticated()` and `ConnectionReceiver::acceptAuthenticated()`.
+
+  Own<AsyncIoStream> stream;
+  // The byte stream.
+
+  Own<PeerIdentity> peerIdentity;
+  // An object indicating who is at the other end of the stream.
+  //
+  // Different subclasses of `PeerIdentity` are used in different situations:
+  // - TCP connections will use NetworkPeerIdentity, which gives the network address of the client.
+  // - Local (unix) socket connections will use LocalPeerIdentity, which identifies the UID, GID,
+  //   and PID of the process that initiated the connection.
+  // - TLS connections will use TlsPeerIdentity which provides details of the client certificate,
+  //   if any was provided.
+  // - When no meaningful peer identity can be provided, `UnknownPeerIdentity` is returned.
+  //
+  // Implementations of `Network`, `ConnectionReceiver`, `NetworkAddress`, etc. should document the
+  // specific assumptions the caller can make about the type of `PeerIdentity`s used, allowing for
+  // identities to be statically downcast if the right conditions are met. In the absence of
+  // documented promises, RTTI may be needed to query the type.
+};
+
+class NetworkPeerIdentity: public PeerIdentity {
+  // PeerIdentity used for network protocols like TCP/IP. This identifies the remote peer.
+  //
+  // This is only "authenticated" to the extent that we know data written to the stream will be
+  // routed to the given address. This doesn not preclude the possibility of man-in-the-middle
+  // attacks by attackers who are able to manipulate traffic along the route.
+public:
+  virtual NetworkAddress& getAddress() = 0;
+  // Obtain the peer's address as a NetworkAddress object. The returned reference's lifetime is the
+  // same as the `NetworkPeerIdentity`, but you can always call `clone()` on it to get a copy that
+  // lives longer.
+
+  static kj::Own<NetworkPeerIdentity> newInstance(kj::Own<NetworkAddress> addr);
+  // Construct an instance of this interface wrapping the given address.
+};
+
+class LocalPeerIdentity: public PeerIdentity {
+  // PeerIdentity used for connections between processes on the local machine -- in particular,
+  // Unix sockets.
+  //
+  // (This interface probably isn't useful on Windows.)
+public:
+  struct Credentials {
+    kj::Maybe<int> pid;
+    kj::Maybe<uint> uid;
+
+    // We don't cover groups at present because some systems produce a list of groups while others
+    // only provide the peer's main group, the latter being pretty useless.
+  };
+
+  virtual Credentials getCredentials() = 0;
+  // Get the PID and UID of the peer process, if possible.
+  //
+  // Either ID may be null if the peer could not be identified. Some operating systems do not
+  // support retrieving these credentials, or can only provide one or the other. Some situations
+  // (like user and PID namespaces on Linux) may also make it impossible to represent the peer's
+  // credentials accurately.
+  //
+  // Note the meaning here can be subtle. Multiple processes can potentially have the socket in
+  // their file descriptor tables. The identified process is the one who called `connect()` or
+  // `listen()`.
+  //
+  // On Linux this is implemented with SO_PEERCRED.
+
+  static kj::Own<LocalPeerIdentity> newInstance(Credentials creds);
+  // Construct an instance of this interface wrapping the given credentials.
+};
+
+class UnknownPeerIdentity: public PeerIdentity {
+public:
+  static kj::Own<UnknownPeerIdentity> newInstance();
+  // Get an instance of this interface. This actually always returns the same instance with no
+  // memory allocation.
+};
+
+// =======================================================================================
+// Accepting connections
+
 class ConnectionReceiver {
   // Represents a server socket listening on a port.
 
 public:
   virtual Promise<Own<AsyncIoStream>> accept() = 0;
   // Accept the next incoming connection.
+
+  virtual Promise<AuthenticatedStream> acceptAuthenticated();
+  // Accept the next incoming connection, and also provide a PeerIdentity with any information
+  // about the client.
+  //
+  // For backwards-compatibility, the default implementation of this method calls `accept()` and
+  // then adds `UnknownPeerIdentity`.
 
   virtual uint getPort() = 0;
   // Gets the port number, if applicable (i.e. if listening on IP).  This is useful if you didn't
@@ -431,6 +533,14 @@ public:
   // Make a new connection to this address.
   //
   // The address must not be a wildcard ("*").  If it is an IP address, it must have a port number.
+
+  virtual Promise<AuthenticatedStream> connectAuthenticated();
+  // Connect to the address and return both the connection and information about the peer identity.
+  // This is especially useful when using TLS, to get certificate details.
+  //
+  // For backwards-compatibility, the default implementation of this method calls `connect()` and
+  // then uses a `NetworkPeerIdentity` wrapping a clone of this `NetworkAddress` -- which is not
+  // particularly useful.
 
   virtual Own<ConnectionReceiver> listen() = 0;
   // Listen for incoming connections on this address.
@@ -808,6 +918,11 @@ public:
   Promise<Own<AsyncIoStream>> accept() override;
   uint getPort() override;
 
+  Promise<AuthenticatedStream> acceptAuthenticated() override;
+  // Always produces UnknownIdentity. Capability-based security patterns should not rely on
+  // authenticating peers; the other end of the capability stream should only be given to
+  // authorized parties in the first place.
+
 private:
   AsyncCapabilityStream& inner;
 };
@@ -835,6 +950,11 @@ public:
 
   Own<NetworkAddress> clone() override;
   String toString() override;
+
+  Promise<AuthenticatedStream> connectAuthenticated() override;
+  // Always produces UnknownIdentity. Capability-based security patterns should not rely on
+  // authenticating peers; the other end of the capability stream should only be given to
+  // authorized parties in the first place.
 
 private:
   kj::Maybe<AsyncIoProvider&> provider;

commit 5aff205b7ebdcb76c8994005cf5fee2c051e8a76

Fix build regression introduced in d3b0dc209 - the SOL_LOCAL is not defined on FreeBSD 11.x and below.

c++/src/kj/async-io-unix.c++
@@ -1487,7 +1487,11 @@ kj::Own<PeerIdentity> SocketAddress::getIdentity(kj::LowLevelAsyncIoProvider& ll
       // MacOS / FreeBSD
       struct xucred creds;
       uint length = sizeof(creds);
+#if defined SOL_LOCAL
       stream.getsockopt(SOL_LOCAL, LOCAL_PEERCRED, &creds, &length);
+#else
+      stream.getsockopt(0, LOCAL_PEERCRED, &creds, &length);
+#endif
       KJ_ASSERT(length == sizeof(creds));
       if (creds.cr_uid != static_cast<uid_t>(-1)) {
         result.uid = creds.cr_uid;